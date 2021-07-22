1cb837fa55f6ddd49641ef0ddb3ffca2a71d8602
Simon Loffler <simon.loffler@acmi.net.au>
2021-07-22 16:04:08 +0930

    #5 Add API speed test

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ help:
 	@echo ' lint             - Lint the code with pylint and flake8 and check imports'
 	@echo '                    have been sorted correctly'
 	@echo ' test             - Run tests'
+	@echo ' speed            - Run a speed test against api.acmi.net.au'
 	@echo ''
 	@echo 'Grouped commands:'
 	@echo ' linttest         - Run lint and test'
@@ -14,4 +15,7 @@ lint:
 test:
 	# Run python tests
 	env `cat /code/config.tmpl.env | xargs` pytest -v -s tests/tests.py
+speed:
+	# Run speed test
+	python3 app/speed_test.py
 linttest: lint test
---
+++ app/speed_test.py
@@ -0,0 +1,50 @@
+import datetime
+import statistics
+
+from furl import furl
+
+from api import ACMI_API_ENDPOINT, XOSAPI
+
+
+class SpeedTest(XOSAPI):
+    """
+    An API speed test that calculates the average response time.
+    """
+    def __init__(self):
+        super().__init__()
+        self.uri = ACMI_API_ENDPOINT
+
+    def start(self, resource='works'):
+        """
+        Get each page of index json files and return the average response time.
+        """
+        average_times = []
+        total_requests = 0
+        start = datetime.datetime.now()
+        total_requests += 1
+        works_json = self.get(resource).json()
+        end = datetime.datetime.now()
+        average_times.append((end - start).total_seconds())
+        self.next_page = works_json.get('next')
+        while self.next_page:
+            params = furl(self.next_page).args
+            start = datetime.datetime.now()
+            total_requests += 1
+            works_json = self.get(resource, params).json()
+            end = datetime.datetime.now()
+            average_times.append((end - start).total_seconds())
+            self.next_page = works_json.get('next')
+        average_request_time = statistics.mean(average_times)
+        print(
+            f'Speed test finished.\nAverage time: {average_request_time} seconds\n'
+            f'Requests: {len(average_times)}/{total_requests}'
+        )
+        return average_request_time
+
+
+if __name__ == '__main__':
+    print('======================')
+    speed_test = SpeedTest()
+    print(f'Starting speed test against: {speed_test.uri}')
+    speed_test.start()
+    print('======================')
